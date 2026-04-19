a3e9cd123014f31251e794a95a468ce9be39a38e
dhaya_py <dhaya251.pur@gmail.com>
2026-04-19 17:57:52 +0530

    banking system task with encapsulation and polymorphism

---
+++ Python-OOP/banking-system.py
@@ -0,0 +1,91 @@
+
+# Inheritance + Ploymorphism
+
+from datetime import datetime
+
+class BankAccount:
+    def __init__(self, name, balance):
+        self.name = name
+        self._balance = balance # private
+        self._transactions = []
+
+    def deposit(self, amount):
+        if amount <= 0:
+            return False, "invalid amount"
+        self.__balance += amount
+        transaction = {
+            "id" : f"T{len(self._transactions)+1}",
+            "type" : "Deposited",
+            "amount" : amount,
+            "time" : datetime.now().strftime('%H:%M')
+        }
+        self._transactions.append(transaction)
+        return True, self._balance
+        
+    def withdraw(self, amount):
+        if amount <= 0:
+            return False, "invalid amount"
+        if amount > self._balance:
+            return False, "Insufficient balance"           
+        self._balance -= amount
+        transaction = {
+            "id" : f"T{len(self._transactions)+1}",
+            "type" : "Withdrew",
+            "amount" : amount,
+            "time" : datetime.now().strftime('%H:%M')
+        }
+        self._transactions.append(transaction)
+        return True, self._balance
+
+    def get_transaction(self):
+        return self._transactions.copy()
+        
+    def get_balance(self):
+        return self._balance
+
+
+class SavingsAccount(BankAccount):
+    def __init__(self, name, balance):
+        super().__init__(name, balance)
+    
+    def add_interest(self, interest):
+        interest = self._balance * interest / 100
+        self._balance += interest
+        return True, self._balance
+
+
+class CurrentAccount(BankAccount):
+    def __init__(self, name, balance, overdraft=5000):
+        super().__init__(name, balance)
+        self.__overdraft = overdraft
+
+    def withdraw(self, amount):
+        if amount <= 0:
+            return False, "Invalid amount"
+        if amount > self._balance + self.__overdraft:
+            return False, "Insufficient balance. overdraft limit exceeded"
+        self._balance -= amount
+        transaction = {
+            "id" : f"T{len(self._transactions)+1}",
+            "Type" : "Withrew",
+            "Amount" : amount,
+            "Time" : datetime.now().strftime('%H:%M')
+        }
+        self._transactions.append(transaction)
+        return True, transaction
+
+acc1 = SavingsAccount("Dhaya", 5000)
+print(acc1.deposit(5000))
+print(acc1.withdraw(2000))
+print(acc1.get_transaction())
+print(acc1.get_balance())
+
+acc2 = CurrentAccount("Dhaya", 5000)
+print(acc2.deposit(5000))
+print(acc2.withdraw(2000))
+print(acc2.get_transaction())
+print(acc2.get_balance())
+
+print(acc1.withdraw(6000))
+print(acc2.withdraw(6000))
+
